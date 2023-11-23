17ea8eceb42983a725127eff28317f1dd8df3817
Leonardo Alonzo <leonardo.a@cliker.mx>
2023-11-23 15:28:18 -0600

    Primer llamado hacia la API

--- src/composables/useClima.js
+++ src/composables/useClima.js
@@ -3,9 +3,10 @@ export default function useClima () {
     const obtenerClima = ({ciudad, pais}) => {
         //Importar el Api Key
         const key = import.meta.env.VITE_API_KEY
-        
-        //Obtener la lat, lng
 
+        //Obtener la lat, lng
+        const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${key}`
+        console.log(url)
         //Obtener el clima
     }
 
