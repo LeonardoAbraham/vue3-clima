05f039e7ceb034c028c05dfd3c2f4a720486c4cf
Leonardo Alonzo <leonardo.a@cliker.mx>
2023-11-23 15:20:06 -0600

    Añadiendo el API Key en variables de entorno

--- src/composables/useClima.js
+++ src/composables/useClima.js
@@ -1,7 +1,12 @@
 
 export default function useClima () {
     const obtenerClima = ({ciudad, pais}) => {
+        //Importar el Api Key
+        const key = import.meta.env.VITE_API_KEY
         
+        //Obtener la lat, lng
+
+        //Obtener el clima
     }
 
     return {
